e92b772a72de452b6f11c3ab124c463849260a3c
Toxblh <toxblh@gmail.com>
2019-09-23 19:51:48 +0100

    install script

---
+++ install-arch-pack.sh
@@ -0,0 +1,22 @@
+yay -S \
+ google-chrome \
+ geary \
+ spotify \
+ lollypop-git \
+ telegram-desktop \
+ discord \
+ slack-desktop \
+ lutris \
+ gwe \
+ cpu-x \
+ qbittorrent \
+ psensor \
+ timeshift \
+ teamviewer \
+ visual-studio-code-bin \
+ simplenote-electron-bin \
+ krita \
+ obs-studi \
+ libreoffice-still \
+ vlc \
+ foliate
