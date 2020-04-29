f506478c32c5b3f1c5a63a0098e98b35b9a3564a
Anton Palgunov <toxblh@gmail.com>
2020-04-29 02:06:02 +0100

    NodeJS

--- install-pack.sh
+++ install-pack.sh
@@ -28,6 +28,7 @@ yay -S vlc
 yay -S foliate
 yay -S libreoffice-still
 yay -S nerd-fonts-fira-code
+yay -S nvm
 
 yay -S inav-configurator-bin
 # Access to usb serial
@@ -85,3 +86,8 @@ sudo usermod -aG ddc $USER
 
 # Gnome OneDark Pro Terminal
 dconf load /org/gnome/terminal/legacy/profiles:/:1430663d-083b-4737-a7f5-8378cc8226d1/ < dotfiles/gnome-terminal/theme-profile.dconf
+
+# NodeJS install
+echo 'source /usr/share/nvm/init-nvm.sh' >> ~/.zshrc
+source ~/.zshrc
+nvm install --lts
